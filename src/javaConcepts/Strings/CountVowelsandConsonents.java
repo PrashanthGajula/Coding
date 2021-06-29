f905114622926d498f7d46148fa602553336d7a9
Prashanth Gajula <prashanth@Prashanths-MacBook-Pro.local>
2021-06-29 12:23:13 -0500

    java strings

---
+++ src/javaConcepts/Strings/CountVowelsandConsonents.java
@@ -0,0 +1,31 @@
+package javaConcepts.Strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountVowelsandConsonents {
+
+	public static void main(String[] args) {
+		String str = "Hello World";
+		Set<Character> set = new HashSet();
+		set.add('a');
+		set.add('e');
+		set.add('i');
+		set.add('o');
+		set.add('u');
+		
+		str = str.toLowerCase();
+		int vowels=0;
+		int consonents=0;
+		for(int i=0;i<str.length();i++) {
+			if(Character.isAlphabetic(str.charAt(i)) && set.contains(str.charAt(i))){
+				vowels++;
+			} else if(Character.isAlphabetic(str.charAt(i))) {
+				consonents++;
+			}
+		}
+System.out.println("vowels = "+vowels);
+System.out.println("consonents = "+consonents);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author prashanth
+ *
+ */
+module coding {
+}
